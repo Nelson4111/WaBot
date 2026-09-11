74904d5c16adb19798c9696acb4cfe9bc50270c0
Morningstar-ex <azelvemorningstar@gmail.com>
2026-09-11 20:27:32 +0700

    Add files via upload

--- plugins/tools/tes.js
+++ plugins/tools/tes.js
@@ -1,11 +1,19 @@
-let handler = async (m) => {
-  let text = `
-*「 🤖 ${global.namebot || 'Avelia'} 」*
+let handler = async (m, { usedPrefix } = {}) => {
+  const prefix = usedPrefix || '.'
 
-Halo! Aku *${global.namebot || 'Avelia'}*, bot asisten aktif milik *${global.author || 'Nenel'}*.  
-Ketik *.menu* untuk melihat semua daftar fitur yang tersedia ✨
-`.trim()
-  await m.reply(text)
+  let text = `
+╭─❏「 *A V E L I A* 」❏\n`
+text += `│ 🤖 *Avelia WA Companion*\n`
+text += `╰─━━━━━━━━━━━━━━─\n\n`
+
+text += `👋 *Halo Semuanya!!*\n`
+text += `> ↳ Aku ${global.namebot || 'Avelia'}, bot asisten aktif milik *'Owner Nenel dan Co-Owner Eza*.\n\n`
+text += `> ↳ Ketik *.menu* untuk melihat semua daftar fitur yang tersedia ✨\n\n`
+text += `> ↳ Chat *.owner* jika ingin Avelia ada di grup kamu juga ✨\n\n`
+
+text += `─━━━━━━━━━━━━━━─`
+
+await m.reply(text)
 }
 
 handler.customPrefix = /^(tes|avelia|nelbot|test|bot)$/i
