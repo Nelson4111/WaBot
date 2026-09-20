7e0e76907439b357ec812cdbaa634333e92735c0
Morningstar-ex <azelvemorningstar@gmail.com>
2026-09-20 18:41:42 +0700

    Add files via upload

--- plugins/system/_autolevelup.js
+++ plugins/system/_autolevelup.js
@@ -2,8 +2,10 @@ import { checkLevelUp } from '../../lib/levelling.js'
 
 export async function before(m) {
     if (!m.sender || m.isBaileys) return
+    const chat = global.db?.data?.chats?.[m.chat]
     let user = global.db?.data?.users?.[m.sender]
-    if (!user || !user.autolevelup) return
+    if (!user || user.autolevelup === false) return
+    if (chat && chat.autolevelup === false) return
 
     try {
         await checkLevelUp(m, this)
